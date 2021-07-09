2c9b934c46cabb2f02d9e5e4cc7002d0174ce850
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-09 19:27:46 +0900

    Update PipeSpawner.cs

--- FlappyBird/Assets/Scripts/PipeSpawner.cs
+++ FlappyBird/Assets/Scripts/PipeSpawner.cs
@@ -22,15 +22,15 @@ public class PipeSpawner : MonoBehaviour
         while (true)
         {
             // 파이프를 생성할 임의의 y값 선택
-            float positionY = Random.Range(-5.68f, 0.0f);
+            float positionY = Random.Range(-5.68f, -1.0f);
             // 아래 파이프 생성 위치
             Vector3 downPosition = new Vector3(4.7f, positionY, 0.0f);
             // 아래 파이프 생성
             GameObject pipeDownClone = Instantiate(pipeDown, downPosition, Quaternion.identity);
             // 아래 파이프 생성 위치
-            Vector3 upPosition = new Vector3(4.7f, positionY + 2.0f, 0.0f);
+            Vector3 upPosition = new Vector3(4.7f, positionY + 10.0f, 0.0f);
             // 위 파이프 생성
-            GameObject pipeUpClone = Instantiate(pipeDown, upPosition, Quaternion.identity);
+            GameObject pipeUpClone = Instantiate(pipeUp, upPosition, Quaternion.identity);
 
             // spawnTime만큼 대기
             yield return new WaitForSeconds(spawnTime);
