e61702e16fe52dd2ebf39552a58280dd663c33b4
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-10 22:03:37 +0900

    Create Bird.cs

---
+++ FlappyBird/Assets/Scripts/Bird.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField]
+    private ScoreBoardSpawner scoreBoardSpawner;
+    [SerializeField]
+    private PositionScroller PositionScroller;
+    [SerializeField]
+    private PipeSpawner pipeSpawner;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 새가 부딪힌 오브젝트의 태그가 "Pipe"혹은 "Ground"이면
+        if (collision.gameObject.tag == "Pipe" || collision.gameObject.tag == "Ground")
+        {
+            StartCoroutine("GameOver");
+
+        }
+    }
+
+    private IEnumerator GameOver()
+    {
+        // 게임 오버
+        Time.timeScale = 0.0f;
+
+
+        scoreBoardSpawner.GameOver();
+        animator.speed = 0.0f;
+        yield return new WaitForSeconds(1.0f);
+
+    }
+
+}
+
