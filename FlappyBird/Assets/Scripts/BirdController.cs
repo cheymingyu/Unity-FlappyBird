f6652b446e3d98086f2a58465b172390d6fe3439
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-12 19:28:16 +0900

    Update BirdController.cs

--- FlappyBird/Assets/Scripts/BirdController.cs
+++ FlappyBird/Assets/Scripts/BirdController.cs
@@ -8,13 +8,14 @@ public class BirdController : MonoBehaviour
     private Movement2D movement2D;
     [SerializeField]
     private float jumpForce = 6.0f;
+    private AudioSource audioSource;
     private Rigidbody2D rigid2D;
 
     private void Awake()
     {
         movement2D = GetComponent<Movement2D>();
         rigid2D = GetComponent<Rigidbody2D>();
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -23,6 +24,7 @@ public class BirdController : MonoBehaviour
         {
             rigid2D.velocity = Vector3.up * jumpForce;
             transform.rotation = Quaternion.Euler(0, 0, 15);
+            audioSource.Play();
         }
         if (rigid2D.velocity.y < -2 && rigid2D.velocity.y > -4)
         {
